35d03fd7381c7115b3599d8d7540969596017cc6
Oeyuu <yu.can.oez@gmail.com>
2025-01-14 09:31:08 +0100

    deleted module css

--- components/aboutus/AboutUsDetails.js
+++ components/aboutus/AboutUsDetails.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from './AboutUsDetails.module.css';
-
 const AboutUsDetails = ({ highlights }) => {
   return (
     <section className={styles.aboutUs}>
